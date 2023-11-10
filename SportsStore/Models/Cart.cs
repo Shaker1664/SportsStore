cc681906fbc0100811dc917a73a05fa0d693601d
shakerkamal <shakerkamal18@gmail.com>
2023-11-11 00:46:37 +0600

    Updated cart razor page model and added unit tests

--- SportsStore/Models/Cart.cs
+++ SportsStore/Models/Cart.cs
@@ -2,5 +2,41 @@
 {
     public class Cart
     {
+        public List<CartLine> Lines { get; set; } = new List<CartLine>();
+        public void AddLine(Product product, int quantity)
+        {
+            CartLine line = Lines
+                .Where(p => p.Product.ProductId == product.ProductId)
+                .FirstOrDefault();
+            if (line == null)
+            {
+                Lines.Add(new CartLine()
+                {
+                    Product = product,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+
+        public void RemoveLine(Product product)
+        {
+            Lines.RemoveAll(l => l.Product.ProductId == product.ProductId);
+        }
+
+        public decimal ComputeTotalValue() =>
+            Lines.Sum(s => s.Product.Price * s.Quantity);
+
+        public void Clear() => Lines.Clear();
+    }
+
+    public class CartLine
+    {
+        public int CartLineId { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
     }
 }
--- SportsStore/Pages/Cart.cshtml.cs
+++ SportsStore/Pages/Cart.cshtml.cs
@@ -1,12 +1,39 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore.Metadata;
+using SportsStore.Contracts;
+using SportsStore.Extensions;
+using SportsStore.Models;
 
 namespace SportsStore.Pages
 {
     public class CartModel : PageModel
     {
-        public void OnGet()
+        private readonly IStoreRepository storeRepository;
+
+        public CartModel(IStoreRepository storeRepository)
         {
+            this.storeRepository = storeRepository;
+        }
+
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+
+        public void OnGet(string returnUrl)
+        {
+            ReturnUrl = returnUrl ?? "/";
+            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+        }
+
+        public IActionResult OnPsot(long productId,  string returnUrl)
+        {
+            Product product = storeRepository.Products
+                .FirstOrDefault(p => p.ProductId == productId);
+
+            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+            Cart.AddLine(product, 1);
+            HttpContext.Session.SetJson("cart", Cart);
+            return RedirectToPage(new { returnUrl = returnUrl });
         }
     }
 }
